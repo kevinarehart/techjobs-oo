ffe80f46a8e1ce342b5e00f238b5259a833df195
Kevin Arehart <kevinarehart@gmail.com>
2017-06-26 19:21:59 -0500

    Fixed ADD feature: displays new added job

--- src/main/java/org/launchcode/controllers/JobController.java
+++ src/main/java/org/launchcode/controllers/JobController.java
@@ -1,15 +1,17 @@
 package org.launchcode.controllers;
 
-import org.launchcode.models.Job;
+import org.launchcode.models.*;
 import org.launchcode.models.forms.JobForm;
 import org.launchcode.models.data.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 /**
  * Created by LaunchCode
@@ -21,11 +23,12 @@ public class JobController {
     private JobData jobData = JobData.getInstance();
 
     // The detail display for a given Job at URLs like /job?id=17
-    @RequestMapping(value = "", method = RequestMethod.GET)
+    @RequestMapping(method = RequestMethod.GET)
     public String index(Model model, int id) {
 
-        /* Completed #1 */
-        model.addAttribute(jobData.findById(id));
+        /* Completed #1: Get job with given id, then view */
+        Job job = jobData.findById(id);
+        model.addAttribute(job);
         return "job-detail";
     }
 
@@ -36,22 +39,22 @@ public class JobController {
     }
 
     @RequestMapping(value = "add", method = RequestMethod.POST)
-    public String add(Model model, @Valid JobForm jobForm, Errors errors) {
+    public String add(Model model, @ModelAttribute @Valid JobForm jobForm, Errors errors) {
 
         /* Completed #6: - Validate JobForm model >
                           -create a new Job >
                            -add to jobData data store >
                            -redirect to job detail view for new Job. */
-        if (errors.hasErrors()){
+        if (errors.hasErrors()) {
             return "new-job";
         }
 
-        Job job =  new Job(jobForm.getName(), jobData.getEmployers().findById(jobForm.getEmployerId()),
-                jobData.getLocations().findById(jobForm.getLocationId()), jobData.getPositionTypes().findById(jobForm.getPositionTypeId()),
+        Job job = new Job(jobForm.getName(),
+                jobData.getEmployers().findById(jobForm.getEmployerId()),
+                jobData.getLocations().findById(jobForm.getLocationId()),
+                jobData.getPositionTypes().findById(jobForm.getPositionTypeId()),
                 jobData.getCoreCompetencies().findById(jobForm.getCoreCompetencyId()));
         jobData.add(job);
-
         return "redirect:/job?id=" + job.getId();
-
     }
 }
